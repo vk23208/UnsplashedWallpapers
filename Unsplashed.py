2c4613124a73718f24d05ba561131a2c7c829b4a
vk23208 <31855976+vk23208@users.noreply.github.com>
2020-02-23 21:20:15 +0530

    Update Unsplashed.py

--- Unsplashed.py
+++ Unsplashed.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 23 19:36:39 2020
-
-@author: vishal
-"""
 #download wallpapers/images from unsplashed site
 
 
@@ -15,19 +9,22 @@ import time
 import os
 from pathlib import Path
 
-input1 = input("Enter image name: ")
+input1 = input("Enter category name: ")
 
+#default URL from unsplashed
 url = 'https://unsplash.com/s/photos/' + str(input1)
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
 
+#taking all 'img' and then bifercating class with its given name
 a = soup.find_all('img', attrs = {'class' : '_2zEKz'})
 
 images = []
 for i in a:
     images.append(i['src'])
 
+#Create a new folder with name given by userinput    
 Path("E:\\picturescrapper\\" + str(input1)).mkdir(parents=True, exist_ok=True)
 
 for i in images:
@@ -36,6 +33,7 @@ for i in images:
     request.urlretrieve(i,full_file_name)    
 
 time.sleep(5)
+#Image folder will open after 5 seconds, meanwhile all images will be downloaded in the path given above
 os.startfile('E:\\picturescrapper')
 
 
